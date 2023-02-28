6c63e214affa18fc7a512b7683f56b9322343709
Telenisis <telenisis1@gmail.com>
2023-02-28 09:31:37 -0500

    all elements align for transition

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let dropDown = document.getElementById("quickBtn");
 let nav = document.getElementById("navbar")
 let main = document.getElementById("main-doc")
+let link = document.getElementById("navClick")
 
 dropDown.addEventListener('click', () => {
-    nav.style.maxHeight = '275px';
-    main.style.marginTop = '270px'
+    nav.style.maxHeight = '409px';
+    main.style.marginTop = '400px';
+    link.style.display = 'block';
+    link.style.height = '326px';
     console.log("hello");
 });
 
